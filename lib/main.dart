5750d6bf47e650006f4481f1bc02a147d690702e
mi6ock <miy606@gmail.com>
2024-06-07 02:07:02 +0900

    トップレベルのファイル群の変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import "package:collection/collection.dart";
 import 'package:flutter/material.dart';
+import 'package:flutter_map_app/app_root.dart';
+import 'package:flutter_map_app/repository/charger_spots_repository.dart';
+import 'package:flutter_map_app/screens/charger_spots/widgets/custom_marker.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:openapi/api.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
+  FlutterError.onError = (details) {
+    FlutterError.dumpErrorToConsole(details);
+  };
   runApp(const MyApp());
 }
 
@@ -10,33 +21,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Map App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Map App Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    const tokyoStation = CameraPosition(target: LatLng(35.681236, 139.767125));
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const GoogleMap(
-        initialCameraPosition: tokyoStation,
+    return ProviderScope(
+          overrides: [
+            baseChargerSpotsRepository.overrideWith((ref) => ref.read(chargerSpotsRepositoryProvider)),
+          ],
+      child: MaterialApp(
+        title: 'Flutter Map App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const AppRoot(),
       ),
     );
   }
